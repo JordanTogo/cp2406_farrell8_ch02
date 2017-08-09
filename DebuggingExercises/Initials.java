95fed458ba31e39caf2c389d0cdde50e774ac13e
Jordan <jordan.togo@my.jcu.edu.au>
2017-08-09 14:55:50 +1000

    complete initials exercise

---
+++ DebuggingExercises/Initials.java
@@ -0,0 +1,8 @@
+public class Initials {
+    public static void main(String[] args) {
+        char firstChar = 'J';
+        char secondChar = 'J';
+        char thirdChar = 'T';
+        System.out.println(firstChar + "." + secondChar + "." + thirdChar);
+    }
+}
--- DebuggingExercises/NauticalMiles.java
+++ DebuggingExercises/NauticalMiles.java
@@ -1,10 +1,12 @@
 public class NauticalMiles {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         float KIL = 1.852f;
         float MIL = 1.150f;
         double nautical = 5.0;
         double resultKm = KIL * nautical;
         double resultM = MIL * nautical;
+
         System.out.println(resultKm + " km's");
         System.out.println(resultM + " miles");
     }
